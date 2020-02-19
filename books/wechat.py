e83825bbc89cbae718d57afabb2e6474029dcc29
victorhongdream <1427298682@qq.com>
2020-02-19 17:26:56 +0800

    up

--- books/wechat.py
+++ books/wechat.py
@@ -27,24 +27,16 @@ class WeChatReader(BaseFeedBook):
     network_timeout       = 60
 
     feeds = [
-        (u'每日学英语', 'https://rsshub.app/wechat/ershicimi/10589', True),
-        (u'进击的Coder', 'https://rsshub.app/wechat/ershicimi/117', True),
-        (u'玩转VS Code', 'https://rsshub.app/wechat/ershicimi/8956', True),
-        (u'Quora文选', 'https://rsshub.app/wechat/ershicimi/10863', True),
-        (u'Python数据科学', 'https://rsshub.app/wechat/ershicimi/3250', True),
-        (u'GitHubDaily', 'https://rsshub.app/wechat/ershicimi/6679', True),
-        (u'码农翻身', 'https://rsshub.app/wechat/ershicimi/57', True),
-        (u'CVer', 'https://rsshub.app/wechat/ershicimi/2698', True),
-        (u'雅思哥', 'https://rsshub.app/wechat/ershicimi/10411', True),
-        (u'程序员小灰', 'https://rsshub.app/wechat/ershicimi/155', True),
-        (u'199IT互联网数据中心', 'https://rsshub.app/wechat/ershicimi/6387', True),
-        (u'非著名程序员', 'https://rsshub.app/wechat/ershicimi/172', True),
+            (u'每日学英语', API_URL % urllib.quote('https://rsshub.app/wechat/ershicimi/10589'), True),
+            (u'进击的Coder', API_URL % urllib.quote('https://rsshub.app/wechat/ershicimi/117'), True),
+            (u'玩转VS Code', API_URL % urllib.quote('https://rsshub.app/wechat/ershicimi/8956'), True),
+            (u'Quora文选', API_URL % urllib.quote('https://rsshub.app/wechat/ershicimi/10863'), True),
+            (u'Python数据科学', API_URL % urllib.quote('https://rsshub.app/wechat/ershicimi/3250'), True),
+            (u'GitHubDaily', API_URL % urllib.quote('https://rsshub.app/wechat/ershicimi/6679'), True),
+            (u'码农翻身', API_URL % urllib.quote('https://rsshub.app/wechat/ershicimi/57'), True),
+            (u'CVer', API_URL % urllib.quote('https://rsshub.app/wechat/ershicimi/2698'), True),
+            (u'雅思哥', API_URL % urllib.quote('https://rsshub.app/wechat/ershicimi/10411'), True),
+            (u'程序员小灰', API_URL % urllib.quote('https://rsshub.app/wechat/ershicimi/155'), True),
+            (u'199IT互联网数据中心', API_URL % urllib.quote('https://rsshub.app/wechat/ershicimi/6387'), True),
+            (u'非著名程序员', API_URL % urllib.quote('https://rsshub.app/wechat/ershicimi/172'), True),
     ]
-    
-    def url4forwarder(self, url):
-        #生成经过转发器的URL
-        return API_URL % urllib.quote(url)
-    
-    def fetcharticle(self, url, opener, decoder):
-        #链接网页获取一篇文章
-        return BaseFeedBook.fetcharticle(self, self.url4forwarder(url), opener, decoder)
